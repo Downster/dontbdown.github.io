781a15a7ba5c4dbb3eef5abc76913fa971b15667
Downster <b.downing.code@gmail.com>
2022-10-04 18:26:46 -0700

    Prod

--- next.config.mjs
+++ next.config.mjs
@@ -1,24 +1,12 @@
-import nextMDX from '@next/mdx'
-import remarkGfm from 'remark-gfm'
-import rehypePrism from '@mapbox/rehype-prism'
-
-/** @type {import('next').NextConfig} */
+/**
+   * @type {import('next').NextConfig}
+   */
 const nextConfig = {
-  pageExtensions: ['jsx', 'mdx'],
-  reactStrictMode: true,
-  swcMinify: true,
-  experimental: {
-    newNextLinkBehavior: true,
-    scrollRestoration: true,
+  images: {
+    loader: 'akamai',
+    path: '',
   },
-}
+  assetPrefix: './',
+};
 
-const withMDX = nextMDX({
-  extension: /\.mdx?$/,
-  options: {
-    remarkPlugins: [remarkGfm],
-    rehypePlugins: [rehypePrism],
-  },
-})
-
-export default withMDX(nextConfig)
+export default nextConfig;
